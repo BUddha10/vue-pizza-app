71d43d74464b1427d2fc89d61e5dce01d762c235
buddha <buddhatamang1000@gmail.com>
2022-02-07 17:43:27 +0545

    create Menu component and complete menu buttons

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <app-header />
     <app-featured />
+    <app-menu />
     <app-footer />
   </div>
 </template>
@@ -10,12 +11,14 @@
 import AppHeader from "@/components/Header.vue";
 import AppFooter from "@/components/Footer.vue";
 import AppFeatured from "@/components/Featured/Index.vue";
+import AppMenu from "@/components/Menu.vue";
 export default {
   name: "App",
   components: {
     AppHeader,
     AppFooter,
     AppFeatured,
+    AppMenu,
   },
 };
 </script>
---
+++ src/components/Menu.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container menu_container">
+    <div class="top">
+      <h2>Menu</h2>
+    </div>
+    <div class="buttons">
+      <md-button
+        v-for="(button, i) in buttons"
+        :key="i"
+        :class="button.active ? 'md-raised' : ''"
+        @click="changeMenu(button.name)"
+      >
+        {{ button.name }}
+      </md-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+  data() {
+    return {
+      buttons: [
+        {
+          name: "pizza",
+          active: true,
+        },
+        {
+          name: "burger",
+          active: false,
+        },
+        {
+          name: "salat",
+          active: false,
+        },
+        {
+          name: "dessert",
+          active: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    changeMenu(value) {
+      this.buttons.forEach((item) => {
+        if (item.name === value) {
+          item.active = true;
+        } else {
+          item.active = false;
+        }
+      });
+    },
+  },
+};
+</script>
