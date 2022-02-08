4e1f00b33f57cd79b631e7e929c8447d318b7e07
buddha <buddhatamang1000@gmail.com>
2022-02-08 13:04:33 +0545

    import all products images on menu card

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,5 +1,5 @@
 <template>
-<!-- eslint-disable prettier/prettier -->
+  <!-- eslint-disable prettier/prettier -->
   <div class="container menu_container">
     <div class="top" v-scroll-reveal.reset>
       <h2>Menu</h2>
@@ -36,6 +36,20 @@
 <script>
 /* eslint-disable prettier/prettier */
 import burger_chicken from "./../assets/images/products/burger_chicken.png";
+import burger_deluxe from "./../assets/images/products/burger_deluxe.png";
+import burger_veggie from "./../assets/images/products/burger_veggie.png";
+import desert_choco from "./../assets/images/products/desert_choco.png";
+import desert_cream from "./../assets/images/products/desert_cream.png";
+import desert_deluxe from "./../assets/images/products/desert_deluxe.png";
+import pizza_bbq from "./../assets/images/products/pizza_bbq.png";
+import pizza_deluxe from "./../assets/images/products/pizza_deluxe.png";
+import pizza_hawai from "./../assets/images/products/pizza_hawai.png";
+import pizza_meat from "./../assets/images/products/pizza_meat.png";
+import pizza_pep from "./../assets/images/products/pizza_pep.png";
+import pizza_veggie from "./../assets/images/products/pizza_veggie.png";
+import salat_chicken from "./../assets/images/products/salat_chicken.png";
+import salat_meat from "./../assets/images/products/salat_meat.png";
+import salat_nuts from "./../assets/images/products/salat_nuts.png";
 export default {
   name: "Menu",
   data() {
@@ -61,14 +75,6 @@ export default {
       products: [
         {
           name: "burger_chicken",
-          type: "pizza",
-          img: burger_chicken,
-          cost: 20,
-          detail:
-            "Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.",
-        },
-        {
-          name: "burger_chicken",
           type: "burger",
           img: burger_chicken,
           cost: 20,
@@ -76,65 +82,113 @@ export default {
             "Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.",
         },
         {
-          name: "burger_chicken",
-          type: "salat",
-          img: burger_chicken,
+          name: "burger_deluxe",
+          type: "burger",
+          img: burger_deluxe,
           cost: 20,
           detail:
             "Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.",
         },
         {
-          name: "burger_chicken",
-          type: "pizza",
-          img: burger_chicken,
+          name: "burger_veggie",
+          type: "burger",
+          img: burger_veggie,
           cost: 20,
           detail:
             "Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.",
         },
         {
-          name: "burger_chicken",
-          type: "pizza",
-          img: burger_chicken,
+          name: "desert_choco",
+          type: "desert",
+          img: desert_choco,
           cost: 20,
           detail:
             "Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.",
         },
         {
-          name: "burger_chicken",
+          name: "desert_cream",
+          type: "desert",
+          img: desert_cream,
+          cost: 20,
+          detail:
+            "Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.",
+        },
+        {
+          name: "desert_deluxe",
           type: "dessert",
-          img: burger_chicken,
+          img: desert_deluxe,
           cost: 20,
           detail:
             "Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.",
         },
         {
-          name: "burger_chicken",
-          type: "burger",
-          img: burger_chicken,
-          cost: 20,
-          detail:
-            "Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.",
-        },
-        {
-          name: "burger_chicken",
+          name: "pizza_bbq",
           type: "pizza",
-          img: burger_chicken,
+          img: pizza_bbq,
           cost: 20,
           detail:
             "Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.",
         },
         {
-          name: "burger_chicken",
+          name: "pizza_deluxe",
+          type: "pizza",
+          img: pizza_deluxe,
+          cost: 20,
+          detail:
+            "Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.",
+        },
+        {
+          name: "pizza_hawai",
+          type: "pizza",
+          img: pizza_hawai,
+          cost: 20,
+          detail:
+            "Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.",
+        },
+        {
+          name: "pizza_meat",
+          type: "pizza",
+          img: pizza_meat,
+          cost: 20,
+          detail:
+            "Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.",
+        },
+        {
+          name: "pizza_pep",
+          type: "pizza",
+          img: pizza_pep,
+          cost: 20,
+          detail:
+            "Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.",
+        },
+        {
+          name: "pizza_veggie",
+          type: "pizza",
+          img: pizza_veggie,
+          cost: 20,
+          detail:
+            "Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.",
+        },
+        {
+          name: "salat_chicken",
           type: "salat",
-          img: burger_chicken,
+          img: salat_chicken,
           cost: 20,
           detail:
             "Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.",
         },
         {
-          name: "burger_chicken",
-          type: "burger",
-          img: burger_chicken,
+          name: "salat_meat",
+          type: "salat",
+          img: salat_meat,
+          cost: 20,
+          detail:
+            "Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.",
+        },
+        {
+          name: "salat_nuts",
+          type: "salat",
+          img: salat_nuts,
           cost: 20,
           detail:
             "Illy Coffee served with a complimentary Leonidas Belgian Chocolate with all beverages.",
